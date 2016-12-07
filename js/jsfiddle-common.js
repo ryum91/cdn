d21ebb10bbf93f767202cbaa3a23dcdeecde1c0c
Charyum Park <ryum91@gmail.com>
2016-12-07 23:11:37 +0900

    [JS] jsfiddle-common

---
+++ js/jsfiddle-common.js
@@ -0,0 +1,14 @@
+function println(text) {
+	var body = document.body;
+	var span = document.createElement('span');
+    span.appendChild(document.createTextNode(text));
+    body.appendChild(span);
+    body.appendChild(document.createElement('br'));
+}
+
+function print(text) {
+	var body = document.body;
+	var span = document.createElement('span');
+	span.appendChild(document.createTextNode(text));
+	body.appendChild(span);
+}
