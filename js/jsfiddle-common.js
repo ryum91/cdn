f832f475eb50a157f3d3f0e28f2c4b0185bbc088
Charyum Park <ryum91@gmail.com>
2016-12-07 23:13:03 +0900

    [JS] jsfiddle-common

--- js/jsfiddle-common.js
+++ js/jsfiddle-common.js
@@ -1,9 +1,9 @@
 function println(text) {
 	var body = document.body;
 	var span = document.createElement('span');
-    span.appendChild(document.createTextNode(text));
-    body.appendChild(span);
-    body.appendChild(document.createElement('br'));
+	span.appendChild(document.createTextNode(text));
+	body.appendChild(span);
+	body.appendChild(document.createElement('br'));
 }
 
 function print(text) {
